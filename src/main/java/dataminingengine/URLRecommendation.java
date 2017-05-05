f011dd80eba7ea8f2528d8897fed5dc485492d38
ChandanaRao13 <chandana.rao13@gmail.com>
2017-05-04 17:53:01 -0700

    Reading multiple collection from mongo

--- src/main/java/dataminingengine/URLRecommendation.java
+++ src/main/java/dataminingengine/URLRecommendation.java
@@ -2,7 +2,9 @@ package dataminingengine;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
@@ -14,6 +16,7 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import com.mongodb.spark.MongoSpark;
+import com.mongodb.spark.config.ReadConfig;
 import com.mongodb.spark.rdd.api.java.JavaMongoRDD;
 
 import scala.Tuple2;
@@ -21,31 +24,42 @@ import scala.Tuple2;
 public class URLRecommendation {
 	public static void main( String[] args ) throws InterruptedException, ParseException
 	{
-		SparkSession spark = SparkSession.builder()
+		SparkSession sparkSessionObject = SparkSession.builder()
 				.master("local")
-				.appName("MongoSparkConnectorIntro")
+				.appName("URLRecommendationComponent")
 				.config("spark.mongodb.input.uri", "mongodb://127.0.0.1/browsinghistory.chromedata")
 				.config("spark.mongodb.output.uri", "mongodb://127.0.0.1/browsinghistory.genericrules")
 				.getOrCreate();
+		
+		JavaSparkContext sparkContextObject = new JavaSparkContext(sparkSessionObject.sparkContext());
 
-		JavaSparkContext sc = new JavaSparkContext(spark.sparkContext());
+		JavaMongoRDD<Document> browsingHistoryRDD = MongoSpark.load(sparkContextObject);
 
-		JavaMongoRDD<Document> rdd = MongoSpark.load(sc);
+		List<Document> filteredRecords =  Arrays.asList(Document.parse("{ $project: {user_id : 1 , _id:0, 'history.hostname':1}}"));
 
-		List<Document> pipeline =  Arrays.asList(Document.parse("{ $project: {user_id : 1 , _id:0, 'history.hostname':1}}"));
+		JavaMongoRDD<Document> aggregatedBrowsingHistoryRDD = browsingHistoryRDD.withPipeline(filteredRecords);
 
-		JavaMongoRDD<Document> aggregatedRdd = rdd.withPipeline(pipeline);
+		List<Document> recordsList = aggregatedBrowsingHistoryRDD.collect();
+		List<String> userId_URL_List = new ArrayList<String>();
 
-		List<Document> list_doc = aggregatedRdd.collect();
-		List<String> userId_urlList = new ArrayList<String>();
-
-		for(Document doc : list_doc) {
-
+		Map<String, String> readOverrides = new HashMap<String, String>();
+		readOverrides.put("collection", "URLRepository");
+		readOverrides.put("readPreference.name", "secondaryPreferred");
+		
+		ReadConfig readConfig = ReadConfig.create(sparkContextObject).withOptions(readOverrides);
+		JavaMongoRDD<Document> URLMappingRDD = MongoSpark.load(sparkContextObject,readConfig);
+		System.out.println("RDD Mapping" + URLMappingRDD.count());
+		
+		JSONParser parser = new JSONParser();
+		JSONObject json;
+		Object ob;
+		
+		for(Document doc : recordsList) {
+			
 			//To fetch single attribute in record
-			JSONParser parser = new JSONParser();
-			JSONObject json = (JSONObject) parser.parse(doc.toJson());			
+			json = (JSONObject) parser.parse(doc.toJson());			
 
-			Object ob = json.get("history");
+			ob = json.get("history");
 			List<Object> history = new ArrayList<Object>((List)ob);
 			List<String> hostnameList = new ArrayList<String>();
 
@@ -53,26 +67,18 @@ public class URLRecommendation {
 				JSONObject hostJson = (JSONObject) parser.parse(host.toString());
 				String hostName = (String) hostJson.get("hostname");
 				hostnameList.add(hostName);
-				userId_urlList.add(json.get("user_id")+","+hostName);
+				userId_URL_List.add(json.get("user_id")+" "+hostName);
 			}
 
 		}
 
-		JavaRDD<String> lines = sc.parallelize(userId_urlList);
+		JavaRDD<String> lines = sparkContextObject.parallelize(userId_URL_List);
 		JavaPairRDD<String,Integer> counts = lines
 				.mapToPair(line -> new Tuple2<>(line,1))
 				.reduceByKey((a,b)->a+b);
-		JavaRDD<String> output = counts.map(a -> a._1 + "," + a._2);
+		JavaRDD<String> output = counts.map(a -> a._1 + " " + a._2);
 		output.saveAsTextFile("output1.txt");
-
-		//aggregatedRdd.saveAsTextFile("/PersonalFiles/MSSE/Semester3/CMPE239/Project/OtherDatasets/sample.txt");
-
-
-
-
-		//		 JavaMongoRDD<Document> aggRDD = rdd.withPipeline(Arrays.asList(Document.parse("{ _id: 5906e78096a09756988390d9}")));
-		//		System.out.println(aggRDD.count());
-		sc.close();
+		sparkContextObject.close();
 
 	}
 }
