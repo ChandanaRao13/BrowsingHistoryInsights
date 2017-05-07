801a71da12818b6e4e72705fcc6df9a366039da0
ChandanaRao13 <chandana.rao13@gmail.com>
2017-05-07 14:50:41 -0700

    Pushing spark changes

--- src/main/java/dataminingengine/URLRecommendation.java
+++ src/main/java/dataminingengine/URLRecommendation.java
@@ -1,117 +1,100 @@
 package dataminingengine;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.mllib.recommendation.ALS;
-import org.apache.spark.mllib.recommendation.MatrixFactorizationModel;
-import org.apache.spark.mllib.recommendation.Rating;
 import org.apache.spark.sql.SparkSession;
 import org.bson.Document;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import com.mongodb.spark.MongoSpark;
 import com.mongodb.spark.config.ReadConfig;
 import com.mongodb.spark.rdd.api.java.JavaMongoRDD;
 
+
 import scala.Tuple2;
 
 public class URLRecommendation {
-	public static void main( String[] args ) throws InterruptedException, ParseException
+	public static void main( String[] args ) throws InterruptedException, IOException, TasteException
 	{
+		// Creating spark configuration 
 		SparkSession sparkSessionObject = SparkSession.builder()
 				.master("local")
 				.appName("URLRecommendationComponent")
-				.config("spark.mongodb.input.uri", "mongodb://127.0.0.1/browsinghistory.chromedata")
+				.config("spark.mongodb.input.uri", "mongodb://127.0.0.1/browsinghistory.h05052017")
 				.config("spark.mongodb.output.uri", "mongodb://127.0.0.1/browsinghistory.genericrules")
 				.getOrCreate();
-		
+
+		// Creating spark context object ( entry point for spark) 
 		JavaSparkContext sparkContextObject = new JavaSparkContext(sparkSessionObject.sparkContext());
-
 		JavaMongoRDD<Document> browsingHistoryRDD = MongoSpark.load(sparkContextObject);
-
 		List<Document> filteredRecords =  Arrays.asList(Document.parse("{ $project: {user_id : 1 , _id:0, 'history.hostname':1}}"));
 		JavaMongoRDD<Document> aggregatedBrowsingHistoryRDD = browsingHistoryRDD.withPipeline(filteredRecords);
 		List<Document> recordsList = aggregatedBrowsingHistoryRDD.collect();
-		
-		List<String> userId_URL_List = new ArrayList<String>();
-		
+
+		// Loading URL Repository collection
 		Map<String, String> readOverrides = new HashMap<String, String>();
 		readOverrides.put("collection", "URLRepository");
 		readOverrides.put("readPreference.name", "secondaryPreferred");
 		ReadConfig readConfig = ReadConfig.create(sparkContextObject).withOptions(readOverrides);
 		JavaMongoRDD<Document> URLMappingRDD = MongoSpark.load(sparkContextObject,readConfig);
-		
-		JSONParser parser = new JSONParser();
+
 		JSONObject json;
-		Object ob;
-		
+		JSONArray historyArray;
+		List<String> userId_URL_List = new ArrayList<String>();
+
+		// Data transformation 
 		for(Document doc : recordsList) {	
 			//To fetch single attribute in record
-			json = (JSONObject) parser.parse(doc.toJson());			
-
-			ob = json.get("history");
-			List<Object> history = new ArrayList<Object>((List)ob);
-			
+			json =  new JSONObject(doc.toJson());
+			historyArray = (JSONArray) json.get("history");
 			List<String> hostnameList = new ArrayList<String>();
-
-			for (Object host : history) {
-				JSONObject chromeDataJSON = (JSONObject) parser.parse(host.toString());
+			for (Object host : historyArray) {
+				JSONObject chromeDataJSON = new JSONObject(host.toString());
 				String hostName = (String) chromeDataJSON.get("hostname");
 				hostnameList.add(hostName);
-				
+
 				String query = "{$match : {URLDomainName : \""+hostName+"\"}}";
 				List<Document> filteredURLRecords = Arrays.asList(Document.parse(query));
 				JavaMongoRDD<Document> aggregatedURLRDD = URLMappingRDD.withPipeline(filteredURLRecords);
 				List<Document> urlRecordsList = aggregatedURLRDD.collect();
-				JSONObject json1 = (JSONObject) parser.parse(urlRecordsList.get(0).toJson());	
-				Object ob1 = json1.get("URLId");
-					
-				userId_URL_List.add(json.get("user_id")+" "+ob1);
+				JSONObject json1 = new JSONObject(urlRecordsList.get(0).toJson());	
+				Object ob1 = json1.get("URLId");			
+				int id = json.get("user_id").toString().hashCode();
+				userId_URL_List.add(id+","+ob1);
 			}
-
 		}
 
+		// Creating RDD
 		JavaRDD<String> lines = sparkContextObject.parallelize(userId_URL_List);
 		JavaPairRDD<String,Integer> counts = lines
 				.mapToPair(line -> new Tuple2<>(line,1))
 				.reduceByKey((a,b)->a+b);
-		JavaRDD<String> output = counts.map(a -> a._1 + " " + a._2);
+		JavaRDD<String> output = counts.map(a -> a._1 + "," + a._2);
 		output.saveAsTextFile("output.txt");
-		
-		JavaRDD<String> data = sparkContextObject.textFile("output.txt");
-        JavaRDD<Rating> ratings = data.map(s -> {
-            String[] sarray = s.split(" ");
-            return new Rating(Integer.parseInt(sarray[0]),
-              Integer.parseInt(sarray[1]),
-              Integer.parseInt(sarray[2]));
-          });
-        
-        int rank = 10;
-        int numIterations = 10;
-        MatrixFactorizationModel model = ALS.train(JavaRDD.toRDD(ratings), rank, numIterations, 0.01);
-        System.out.println("*******");
-        //System.out.println("-----" + model.predict(2, 3) + "------" + model.recommendProductsForUsers(2));
-        JavaRDD<Tuple2<Object, Rating[]>> products = model.recommendProductsForUsers(7).toJavaRDD();
-       
-        for (Tuple2<Object, Rating[]> rule
-                : products.collect()) {
-        		for(Rating rate : rule._2){
-                System.out.println("User:::" + rule._1 + " ------  Chance of accessing::" + Math.round(rate.rating()) + " ----- URL:::" + rate.product() ) ;
-        		}
-                  
-              }
-		
+
 		
 		sparkContextObject.close();
-
 	}
 }
+
